148868dcd525a23593c97903e2e1a740995550e4
Walter Clayton <walthedude@gmail.com>
2020-03-17 14:07:32 +0000

    fixed bugs with rendering + made CRUD!

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -14,6 +15,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
