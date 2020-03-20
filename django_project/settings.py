c8f5032f28da82802ebfdc04911648db38162637
Walter Clayton <walthedude@gmail.com>
2020-03-20 11:46:32 +0000

    turned debug to false

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['stark-taiga-57830.herokuapp.com']
 
